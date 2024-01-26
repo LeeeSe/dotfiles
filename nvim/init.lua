1649b2cbc92d836e2e791bfb184091cb486c6b2c
LeeeSe <724818342@qq.com>
2024-01-27 03:06:25 +0800

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,8 @@ if vim.g.neovide then
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.keymap.set("n", "<D-j>", "<C-d>") -- quickly down
+  vim.keymap.set("n", "<D-k>", "<C-u>") -- quickly up
 end
 
 -- Allow clipboard copy paste in neovim
