0a6cae655a53f03129e4b744fe71f04287930eaf
LeeeSe <44465325+LeeeSe@users.noreply.github.com>
2025-03-05 07:53:30 +0000

    update

--- fish/config.fish
+++ fish/config.fish
@@ -4,25 +4,35 @@ end
 
 alias build="cargo build --release"
 alias run="cargo run --release"
+alias debug="cargo run"
 alias clone="git clone"
 alias vif="vi ~/.config/fish/config.fish"
 alias via="vi ~/.config/alacritty/alacritty.toml"
-alias vi="neovide"
 alias l="ls"
 alias vir="vi ~/.config/rio/config.toml"
+alias cl="clear"
+alias icat="kitten icat"
+alias cd="z"
+alias cd..="cd .."
+alias s="kitten ssh"
+alias chat=hambur
 
-# export https_proxy=http://127.0.0.1:6152;export http_proxy=http://127.0.0.1:6152;export all_proxy=socks5://127.0.0.1:6153
+export https_proxy=http://127.0.0.1:7890
+export http_proxy=http://127.0.0.1:7890
+export all_proxy=socks5://127.0.0.1:7890
+
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/cmd:$PATH"
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-export OPENAI_API_KEY="sk-NhQW7P6Hficr00BF49CdA1E684Fd497eBfD93015105fB117"
-export COPILOT_API_KEY="ghu_0aykjA2NsIhw7jle3xC8CNBPTtsqSu15ZbNB"
-export HANDLER="copilot"
+export PATH="$PATH:/Users/ls/.orbstack/bin"
 
+export OPENAI_API_KEY=ef52cb82-7e78-413f-8736-373613357e01
+export OPENROUTER_API_KEY=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
 
-# Setting PATH for Python 3.12
-# The original version is saved in /Users/ls/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
+zoxide init fish | source
+
+# Added by Windsurf
+fish_add_path /Users/ls/.codeium/windsurf/bin
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
